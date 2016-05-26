35b0e9108cb7b33af03a9c0ced1dfd36cadf67c5
Enoch Carter <enoch.carter@vivint.com>
2016-05-26 08:51:54 -0600

    changed QMetaType::Type T to int to allow registration of custom QMetaTypes

--- src/msgpack.h
+++ src/msgpack.h
@@ -13,7 +13,7 @@ namespace MsgPack
     MSGPACK_EXPORT bool registerUnpacker(qint8 msgpackType, unpack_user_f unpacker);
 
     MSGPACK_EXPORT QByteArray pack(const QVariant &variant);
-    MSGPACK_EXPORT bool registerPacker(QMetaType::Type qType, qint8 msgpackType, pack_user_f packer);
+    MSGPACK_EXPORT bool registerPacker(int qType, qint8 msgpackType, pack_user_f packer);
     MSGPACK_EXPORT qint8 msgpackType(int qType);
 
     MSGPACK_EXPORT bool registerType(QMetaType::Type qType, quint8 msgpackType);
